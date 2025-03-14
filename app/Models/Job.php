cddfa31e2ff0166afb1b9c12fd6122824e09f586
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-03-14 16:18:09 +0900

    creating migration files

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,8 +7,15 @@ use Illuminate\Support\Arr;
 
 class Job
 {
+
+    /**
+     * すべての求人情報を取得する。
+     * 
+     * @return array 求人情報のリスト（各求人はID、タイトル、給与を含む）。
+     */
     public static function all(): array
     {
+        // 求人情報の配列を返す（データは固定）。
         return  [
             [
                 'id' => 1,
@@ -28,14 +35,24 @@ class Job
         ];
     }
 
+    /**
+     * Find a job by its ID.
+     * 
+     * @param int $id the Id of the job to find
+     * @return array The job data is found
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If the job is not found.
+     */
     public static function find(int $id): array
     {
+        // Search for the first job in the list that matches the given ID
         $job =   Arr::first(static::all(), fn($job) => $job['id'] == $id);
 
+        // If no match job is found, return a 404 error
         if (! $job) {
             abort(404);
         }
 
+        // Return the found job data
         return $job;
     }
 }
