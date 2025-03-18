14caf395921c9d9e5ed1ca4844e5c5381e221869
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-03-18 10:39:22 +0900

    create database and add Job models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,56 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Job
+/**
+ * Job モデルクラス
+ * 
+ * このクラスは "job_listings"テブールに対応する Eloquentモデルです。
+ */
+class Job extends Model
 {
+    // 使用するデータベースのテブール名を指定
+    protected $table = 'job_listings';
 
-    /**
-     * すべての求人情報を取得する。
-     * 
-     * @return array 求人情報のリスト（各求人はID、タイトル、給与を含む）。
-     */
-    public static function all(): array
-    {
-        // 求人情報の配列を返す（データは固定）。
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Designer',
-                'salary' => '$35,000'
-            ]
-        ];
-    }
-
-    /**
-     * Find a job by its ID.
-     * 
-     * @param int $id the Id of the job to find
-     * @return array The job data is found
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If the job is not found.
-     */
-    public static function find(int $id): array
-    {
-        // Search for the first job in the list that matches the given ID
-        $job =   Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        // If no match job is found, return a 404 error
-        if (! $job) {
-            abort(404);
-        }
-
-        // Return the found job data
-        return $job;
-    }
+    // 一括代入を許可するカラムを指定
+    protected $fillable = ['title', 'salary'];
 }
