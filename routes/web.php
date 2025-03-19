313b346c49adde386b37c56faaa1874afcd5b3a3
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-03-19 11:26:18 +0900

    group route

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 
 // Job routes
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{id}', [JobController::class, 'destroy']);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
 // Contact route
 Route::view('/contact', 'contact');
