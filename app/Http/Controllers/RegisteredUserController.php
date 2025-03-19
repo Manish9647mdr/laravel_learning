748666924df285387d355b11c0b72a24b4a96951
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-03-19 16:46:45 +0900

    make login and registration form part 2

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,18 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
+    // Show the registration form
     public function create()
     {
         return view('auth.register');
     }
 
+    // Handle user registration 
     public function store()
     {
-        // return view('auth.login');
-        dd(request()->all());
+        // Validate the input data
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        // Create a new user
+        $user = User::create($attributes);
+
+        // Log in the user
+        Auth::login($user);
+
+        // Redirect to the jobs page
+        return redirect('/jobs');
     }
 }
